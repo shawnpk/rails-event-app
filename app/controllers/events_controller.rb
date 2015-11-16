5e941a9c352e687cc6f6343f28d624795ac99b27
Shawn Kearney <shawnpk@ineptsoftware.com>
2015-11-16 12:44:20 -0800

    Add new event functionality

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,19 +1,31 @@
 class EventsController < ApplicationController
 
+  before_action :find_event, only: [:show, :edit, :update]
+
   def index
     @events = Event.all
   end
 
   def show
-    @event = Event.find(params[:id])
+    
+  end
+
+  def new
+    @event = Event.new
   end
 
   def edit
-    @event = Event.find(params[:id])
+    
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save
+    redirect_to @event
   end
 
   def update
-    @event = Event.find(params[:id])
+    
     @event.update(event_params)
     redirect_to @event
   end
@@ -22,6 +34,10 @@ class EventsController < ApplicationController
     def event_params
       params.require(:event).
         permit(:name, :description, :location, :price, :starts_at)
+    end
+
+    def find_event
+      @event = Event.find(params[:id])
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
   root 'events#index'
-  get 'events',          to: 'events#index'
-  get 'events/:id',      to: 'events#show', as: 'event'
-  get 'events/:id/edit', to: 'events#edit', as: 'edit_event'
-  patch 'events/:id',    to: 'events#update'
+  # get 'events',          to: 'events#index'
+  # get 'events/new',      to: 'events#new'
+  # get 'events/:id',      to: 'events#show', as: 'event'
+  # get 'events/:id/edit', to: 'events#edit', as: 'edit_event'
+  # patch 'events/:id',    to: 'events#update'
+  resources :events
 
 end
