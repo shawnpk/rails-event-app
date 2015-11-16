b1c9c4c9a2244364f8ee61193d9b242b2276a635
Shawn Kearney <shawnpk@ineptsoftware.com>
2015-11-16 10:48:29 -0800

    Add edit form

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'events#index'
   get 'events',   to: 'events#index'
   get 'events/:id', to: 'events#show', as: 'event'
+  get 'events/:id/edit', to: 'events#edit', as: 'edit_event'
 
 end
