c9c421d214286f94c316b91eaf3704ceccf2f6f1
Shawn Kearney <shawnpk@ineptsoftware.com>
2015-11-16 14:10:12 -0800

    Add images for events

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -9,4 +9,12 @@ module EventsHelper
     end
   end
 
+  def image_for(event)
+    if event.image_file_name.blank?
+      image_tag '5766004.png'
+    else
+      image_tag event.image_file_name
+    end
+  end
+
 end
