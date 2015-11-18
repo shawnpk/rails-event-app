00da1507d4c69e6fbf1dc28c82969a3fb547f58a
Shawn Kearney <shawnpk@ineptsoftware.com>
2015-11-17 16:16:58 -0800

    Add event registration functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :registrations
   root 'events#index'
-  # get 'events',          to: 'events#index'
-  # get 'events/new',      to: 'events#new'
-  # get 'events/:id',      to: 'events#show', as: 'event'
-  # get 'events/:id/edit', to: 'events#edit', as: 'edit_event'
-  # patch 'events/:id',    to: 'events#update'
-  resources :events
+  
+  resources :events do
+    resources :registrations
+  end
 
 end
